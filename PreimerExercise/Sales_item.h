1557379e638be5dc6e962fb73ade53ea37e7c275
LinQiang391 <1667704220@qq.com>
2022-08-11 23:07:17 +0800

    加法运算符重载

--- PreimerExercise/Sales_item.h
+++ PreimerExercise/Sales_item.h
@@ -6,24 +6,26 @@ class Sales_item
 public:
 	Sales_item();
 	~Sales_item();
-    friend std::ostream& operator<<(std::ostream& output, Sales_item& r)
+    string ISBN;
+    int Sale_nums;
+    float Price;
+    //Sales_item operator+(Sales_item s1);
+    friend Sales_item operator+(Sales_item& s1, Sales_item& s2);
+    friend ostream& operator<<(ostream& output, Sales_item& r)
     {
-        output << "width: " << r.ISBN << ", ";
-        output << "height: " << r.Sale_nums << ", ";
-
+        output << r.ISBN << " " << r.Sale_nums << " " << r.Sale_nums*r.Price << " " << r.Price;
         return output;
     }
 
-    friend std::istream& operator>>(std::istream& input, Sales_item& r)
+    friend istream& operator>>(istream& input, Sales_item& r)
     {
-        input >> r.ISBN >> r.Sale_nums;
+        input >> r.ISBN >> r.Sale_nums>>r.Price;
         return input;
     }
-	string ISBN;
-	int Sale_nums;
-	float Total;
-private:
 
+
+private:
+ 
 };
 
 Sales_item::Sales_item()
@@ -33,5 +35,21 @@ Sales_item::Sales_item()
 
 Sales_item::~Sales_item()
 {
+}
+//Sales_item Sales_item::operator+(Sales_item s1) {
+//    Sales_item temp;
+//    temp.ISBN = this->ISBN;
+//    temp.Sale_nums = this->Sale_nums + s1.Sale_nums;
+//    //temp.Sale_total = this->Sale_total + s1.Sale_total;
+//    temp.Price = (this->Price * this->Sale_nums + s1.Price * s1.Sale_nums) / temp.Sale_nums;
+//    return temp;
+//}
+Sales_item operator+(Sales_item& s1,Sales_item& s2) {
+    Sales_item temp;
+    temp.ISBN = s1.ISBN;
+    temp.Sale_nums = s1.Sale_nums + s2.Sale_nums;
+    //temp.Sale_total = this->Sale_total + s1.Sale_total;
+    temp.Price = (s1.Price *s1.Sale_nums + s2.Price * s2.Sale_nums) / temp.Sale_nums;
+    return temp;
 }
 
--- PreimerExercise/Test.cpp
+++ PreimerExercise/Test.cpp
@@ -1,7 +1,9 @@
-#include"Sales_item.h"
 #include<iostream>
+#include"Sales_item.h"
 void main() {
-	Sales_item item;
-	cin >> item;
-	cout << item;
+	Sales_item book1,book2,book3;
+	cin >> book1>>book2;
+	//book3 = book1.operator+(book2);
+	book3 = book1 + book2;
+	cout << book3 <<endl;
 }
